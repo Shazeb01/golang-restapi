c6e02400c808b8da11a5b89d80c88ab4b7b1ce20
Shazeb01 <shazebvps@gmail.com>
2020-09-22 01:22:16 -0700

    sqlx

--- main.go
+++ main.go
@@ -3,60 +3,60 @@ package main
 import (
 	"database/sql"
 	"fmt"
-        "log"
-        "net/http"
+	"log"
+	"net/http"
 
-        _ "github.com/go-sql-driver/mysql"
-        "github.com/gorilla/mux"
-        "github.com/jmoiron/sqlx"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
 )
 
 var err error
 var db *sqlx.DB
 
 type Customers struct {
-        CustomerNumber         string `json:"customerNumber"`
-        CustomerName           string `json:"customerName"`
-        ContactLastName        string `json:"contactLastName"`
-        ContactFirstName       string `json:"contactFirstName"`
-        Phone                  string `json:"phone"`
-        AddressLine1           string `json:"addressLine1"`
-        AddressLine2           sql.NullString
-        City                   string `json:"city"`
-        State                  sql.NullString
-        PostalCode             string `json:"postalCode"`
-        Country                string `json:"country"`
-        SalesRepEmployeeNumber sql.NullString
-        CreditLimit            string `json:"creditLimit"`
+	CustomerNumber         string `json:"customerNumber"`
+	CustomerName           string `json:"customerName"`
+	ContactLastName        string `json:"contactLastName"`
+	ContactFirstName       string `json:"contactFirstName"`
+	Phone                  string `json:"phone"`
+	AddressLine1           string `json:"addressLine1"`
+	AddressLine2           sql.NullString
+	City                   string `json:"city"`
+	State                  sql.NullString
+	PostalCode             string `json:"postalCode"`
+	Country                string `json:"country"`
+	SalesRepEmployeeNumber sql.NullString
+	CreditLimit            string `json:"creditLimit"`
 }
 
 func main() {
-        db, err := sqlx.Open("mysql", "root:127.0.0.1:3306)/classicmodels")
+	db, err := sqlx.Open("mysql", "root:(127.0.0.1:3306)/classicmodels")
 
-        if err != nil {
-                panic(err.Error())
-        }
-        defer db.Close()
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
 
-        router := mux.NewRouter()
-        router.HandleFunc("/data", getPosts).Methods("GET")
+	router := mux.NewRouter()
+	router.HandleFunc("/data", getPosts).Methods("GET")
 
-        http.ListenAndServe(":4444", router)
+	http.ListenAndServe(":4444", router)
 }
 
 func getPosts(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
-        post := Customers{}
-        rows, err := db.Queryx("SELECT * FROM customers")
-        if err != nil {
-                log.Fatal(err)
-        }
-        for rows.Next() {
-                err := rows.StructScan(&post)
-                if err != nil {
-                        log.Fatalln(err)
-                }
-                fmt.Printf("%#v\n", post)
-        }
+	post := Customers{}
+	rows, err := db.Queryx("SELECT * FROM customers")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		err := rows.StructScan(&post)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Printf("%#v\n", post)
+	}
 }
